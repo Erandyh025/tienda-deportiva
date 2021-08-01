2c4e4757f0ecc965ca97c8717dde5571cce8d0c6
unknown <20183l301027@utcv.edu.mx>
2021-08-01 17:41:52 -0500

    se agrego la ruta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CatalogoComponent } from './catalogo/catalogo.component';
+import { InfoComponent } from './info/info.component';
 import { InicioComponent } from './inicio/inicio.component';
 
 const routes: Routes = [
   {path: '', component: InicioComponent},
-  {path: 'catalogo', component: CatalogoComponent}
+  {path: 'catalogo', component: CatalogoComponent},
+  {path: 'info', component: InfoComponent}
 ];
 
 @NgModule({
