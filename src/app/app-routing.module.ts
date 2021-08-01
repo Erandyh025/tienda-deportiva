0cbe12d030a8ea147ea86e6440c268a4d50d55fb
Peter BPerez <peter171199.pbp@gmail.com>
2021-08-01 18:10:12 -0500

    Se agrego la ruta en el app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CatalogoComponent } from './catalogo/catalogo.component';
+import { ContactoComponent } from './contacto/contacto.component';
 import { InfoComponent } from './info/info.component';
 import { InicioComponent } from './inicio/inicio.component';
 
 const routes: Routes = [
   {path: '', component: InicioComponent},
   {path: 'catalogo', component: CatalogoComponent},
-  {path: 'info', component: InfoComponent}
+  {path: 'info', component: InfoComponent},
+  {path: 'contacto', component: ContactoComponent}
 ];
 
 @NgModule({
